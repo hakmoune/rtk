fadfb0cf1484664c3dacbe7303427f2888aa0bd6
EL MAHDI HAKMOUNE <el-mahdi.hakmoune@fulll.fr>
2025-01-31 01:11:44 +0100

    createAsyncThunk: handle update

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -1,15 +1,31 @@
-import { Button } from "@mui/material";
+import { Button, TextField } from "@mui/material";
 import { TProduct } from "../state/products/types";
 import { useDispatch } from "react-redux";
-import { deleteProductById } from "../state/products/productsSlice";
+import {
+  deleteProductById,
+  updateProductById,
+} from "../state/products/productsSlice";
 import { AppDispatch } from "../state/store";
+import { useState } from "react";
 
 const Product = ({ product }: { product: TProduct }) => {
+  const [value, setValue] = useState<string>(product.title);
   const dispatch: AppDispatch = useDispatch();
 
   return (
-    <li>
-      {product.title}
+    <li style={{ listStyle: "none", marginBottom: "10px" }}>
+      <TextField
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+        variant="outlined"
+      />
+      <Button
+        onClick={() =>
+          dispatch(updateProductById({ id: product.id, title: value }))
+        }
+      >
+        Update
+      </Button>
       <Button onClick={() => dispatch(deleteProductById(product))}>
         Delete
       </Button>
--- src/state/products/productsSlice.ts
+++ src/state/products/productsSlice.ts
@@ -9,16 +9,17 @@ const initialState: IinitialState = {
 
 export const fetchProducts = createAsyncThunk(
   "products/fetchProducts", // Used to generate unique action types (pending, fulfilled, rejected).
-  async (_, thunkAPI) => {
+  async (_, { rejectWithValue }) => {
     try {
       const response = await fetch("https://dummyjson.com/products");
 
       if (!response.ok) {
-        throw new Error("Server Error: FAILED TO FETCH PRODUCTS"); // Payload will be available in action.payload.
+        return rejectWithValue("Server Error: FAILED TO DELETE PRODUCT"); // Payload will be available in action.payload.
       }
       return (await response.json()).products; // Payload will be available in action.payload.
     } catch (error: any) {
-      return thunkAPI.rejectWithValue(error.message);
+      console.error(error);
+      return rejectWithValue(error.message);
     }
   }
 );
@@ -31,19 +32,49 @@ export const deleteProductById = createAsyncThunk(
   ) => {
     try {
       const response = await fetch(
-        `https://dummyjson.com/products/${product.id}`,
+        `https://dummyjson.com/productsss/${product.id}`,
         {
           method: "DELETE",
         }
       );
 
       if (!response.ok) {
-        throw new Error("Server Error: FAILED TO DELETE PRODUCT");
+        return rejectWithValue("Server Error: FAILED TO DELETE PRODUCT");
       }
 
       return await response.json();
     } catch (error: any) {
-      rejectWithValue(error.message);
+      console.error(error);
+      return rejectWithValue(error.message);
+    }
+  }
+);
+
+export const updateProductById = createAsyncThunk(
+  "products/updateProductById",
+  async (
+    { id, title }: { id: number; title: string }, // single payload argument
+    { rejectWithValue }: { rejectWithValue: (value: any) => void } // ThunkAPI object (which includes rejectWithValue, dispatch, getState...)
+  ) => {
+    try {
+      const response = await fetch(`https://dummyjson.com/products/${id}`, {
+        method: "PUT",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ title }),
+      });
+
+      if (!response.ok) {
+        // This returns early with rejectWithValue, so the catch block is never executed
+        // The error is directly passed to Redux via action.payload.
+        // Redux dispatches the "rejected action" with "action.payload" containing "Server Error: FAILED TO UPDATE PRODUCT"
+        return rejectWithValue("Server Error: FAILED TO UPDATE PRODUCT");
+      }
+
+      return await response.json();
+    } catch (error: any) {
+      // Catch used if you want to catch network errors (e.g., no internet) or unexpected errors (e.g., bad JSON parsing):
+      console.error(error);
+      return rejectWithValue(error.message);
     }
   }
 );
@@ -79,6 +110,31 @@ const productsSlice = createSlice({
         );
       })
       .addCase(deleteProductById.rejected, (state, action) => {
+        state.status = "failed";
+        state.error = action.payload as string;
+      });
+
+    // Update product by id
+    builder
+      .addCase(updateProductById.pending, (state) => {
+        state.status = "loading";
+      })
+      .addCase(updateProductById.fulfilled, (state, action) => {
+        state.status = "succeeded";
+
+        const index = state.products.findIndex(
+          (product) => product.id === action.payload.id
+        );
+
+        if (index !== -1) {
+          // Redux toolkit uses Immer.js to allow direct state mutation.
+          // Redux  keeps the state immutable in the background.
+          // Immer automatically creates a new state behind the scenes.
+          // you don't have to manually create a new array to ensure immutability
+          state.products[index] = action.payload;
+        }
+      })
+      .addCase(updateProductById.rejected, (state, action) => {
         state.status = "failed";
         state.error = action.payload as string;
       });
